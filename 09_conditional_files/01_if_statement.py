7c40362150d995518afccf5c8abf3dd2d9f0e009
ganesh-bhale <bhaleg22@gmail.com>
2026-02-24 23:33:10 +0530

    Initial commit

---
+++ 09_conditional_files/01_if_statement.py
@@ -0,0 +1,6 @@
+a=int(input(" Enter your age"))
+
+if(a>22):
+    print("You can drive")
+
+print("End of program")
---
+++ 09_conditional_files/02_if_else.py
@@ -0,0 +1,11 @@
+age=int(input("enter your age"))
+
+if(age>22):
+    print("you can drive ")
+
+else:
+    print("You cannot drive")
+
+print("This line is not in condition")
+
+# this condition is applied to that which has identation 
