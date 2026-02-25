452604d95a60ddd88ddf23026186c53fd3fb4f72
ganesh-bhale <bhaleg22@gmail.com>
2026-02-25 22:49:42 +0530

    11_practice

---
+++ 11_practice/01_question.py
@@ -0,0 +1,8 @@
+# use of loop if // else
+
+a=int(input("enter a number"))
+
+if (a<10):
+    print(" you are to young ")
+else:
+     print("you are older")
---
+++ 11_practice/02_question.py
@@ -0,0 +1,7 @@
+# table of 10 , using while loop
+
+i=1
+
+while(i<11):
+    print("10 x",i,"=",10*i)
+    i=i+1
